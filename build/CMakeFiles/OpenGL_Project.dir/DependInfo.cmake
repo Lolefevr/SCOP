23ee7fd67ce48e7e736ee3a030ff6eb103ae0fbf
lolefevr <lolefevr@student.42perpignan.fr>
2024-08-08 14:54:27 +0200

    Update1

---
+++ build/CMakeFiles/OpenGL_Project.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zhakhann/lolefevr/SCOP/src/main.cpp" "CMakeFiles/OpenGL_Project.dir/src/main.cpp.o" "gcc" "CMakeFiles/OpenGL_Project.dir/src/main.cpp.o.d"
+  "/home/zhakhann/lolefevr/SCOP/src/shader.cpp" "CMakeFiles/OpenGL_Project.dir/src/shader.cpp.o" "gcc" "CMakeFiles/OpenGL_Project.dir/src/shader.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
